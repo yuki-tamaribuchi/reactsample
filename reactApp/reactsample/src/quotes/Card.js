c39f19fe75322cf0d703710bc9d86a87b09eb476
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-07-24 19:57:33 +0900

    Modify buttons for redux

--- reactApp/reactsample/src/quotes/Card.js
+++ reactApp/reactsample/src/quotes/Card.js
@@ -1,9 +1,15 @@
 function Card(props) {
 	return (
 		<div className="card">
-			<div className="buttons">
-				<button onClick={()=>props.deleteCard(props.id)}>Delete</button>
-			</div>
+			{props.saved ? (
+				<div></div>
+			):(
+				<div className="buttons">
+					<button onClick={()=>props.saveCard(props.id)}>Save</button>
+					<button onClick={()=>props.deleteCard(props.id)}>Delete</button>
+				</div>
+			)}
+			
 			<p>{props.data.content}</p>
 			<div className="originatorName">
 				{props.data.originator.name}
