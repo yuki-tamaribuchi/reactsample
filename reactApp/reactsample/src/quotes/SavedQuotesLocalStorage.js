58a70e9796d188c30637831ec80d62bedbc25e69
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-07-25 08:55:15 +0900

    Modify dnd

--- reactApp/reactsample/src/quotes/SavedQuotesLocalStorage.js
+++ reactApp/reactsample/src/quotes/SavedQuotesLocalStorage.js
@@ -3,40 +3,41 @@ import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import DraggableCard from "./DraggableCard";
 
+
 function SavedQuotesLocalStorage() {
-	const [savedQuoteCardList, setSavedQuotesCardList] = useState([]);
-	const moveQuoteCardListItem = useCallback((dragIndex, hoverIndex)=>{
-		const dragItem = savedQuoteCardList[dragIndex]
-		const hoverItem = savedQuoteCardList[hoverIndex]
-		setSavedQuotesCardList(prevQuoteCardList=>{
-			const updateSavedQuotesCardList = [...prevQuoteCardList]
-			updateSavedQuotesCardList[dragIndex] = hoverItem
-			updateSavedQuotesCardList[hoverIndex] = dragItem
-			return updateSavedQuotesCardList
-		})
-	},[savedQuoteCardList])
-
-	function createSavedQuoteCardList() {
-		let cardId = 0;
-		const cardList = [];
-		const localStorageJSONString=localStorage.getItem("dataList");
-		if (localStorageJSONString!=null) {
-			JSON.parse(localStorageJSONString).forEach(item=>{
-				cardList.push(<DraggableCard data={item} key={cardId} id={cardId} index={cardId} moveListItem={moveQuoteCardListItem} />)
-				cardId+=1;
-			})
-		}
-		
-		return cardList;
+	const localStorageJSONString=localStorage.getItem("dataList");
+	let parsedCardDataList = null;
+	if (localStorageJSONString!=null) {
+		parsedCardDataList = JSON.parse(localStorageJSONString);
 	}
+	const [cardDataList, setCardDataList] = useState(parsedCardDataList);
+	
+	const moveCardListItem = useCallback(
+		(dragIndex, hoverIndex) => {
+			const dragItem = cardDataList[dragIndex]
+			const hoverItem = cardDataList[hoverIndex]
+			setCardDataList(cards=>{
+				//three dots is used for deep copy(?)
+				const updatedCards = [...cards]
+				updatedCards[dragIndex] = hoverItem
+				updatedCards[hoverIndex] = dragItem
+				return updatedCards
+			})
+		},
+		[cardDataList]
+	)
 
-	useEffect(()=>{
-		setSavedQuotesCardList(createSavedQuoteCardList())
-	},[])
 
 	return (
 		<DndProvider backend={HTML5Backend}>
-			{savedQuoteCardList}
+			{cardDataList.map((cardData, index)=>(
+				<DraggableCard
+					key={index}
+					index={index}
+					data={cardData}
+					moveListItem={moveCardListItem}
+				/>
+			))}
 		</DndProvider>
 	)
 }
